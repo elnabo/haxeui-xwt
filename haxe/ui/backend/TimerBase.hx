aeba0bb7128c8c64e0e7266e554204848264f052
Valentin Lemière <valentin@ibilon.com>
2016-12-09 19:06:26 +0100

    Implement TimerBase

--- haxe/ui/backend/TimerBase.hx
+++ haxe/ui/backend/TimerBase.hx
@@ -2,12 +2,18 @@ package haxe.ui.backend;
 
 class TimerBase
 {
+	var _timer : cs.system.IDisposable;
+
 	function new (delay:Int, callback:Void->Void)
 	{
-		callback();
+		_timer = xwt.Application.TimeoutInvoke(delay, function () {
+			callback();
+			return false; // discard the timeout after use
+		});
 	}
 
 	function stop ()
 	{
+		_timer.Dispose();
 	}
 }
