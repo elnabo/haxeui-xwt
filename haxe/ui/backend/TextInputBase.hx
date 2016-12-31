4f354ff65282bc91cd5d30b909c399392437d28e
Valentin Lemière <valentin@ibilon.com>
2016-12-31 11:35:20 +0100

    Test travis trigger

--- haxe/ui/backend/TextInputBase.hx
+++ haxe/ui/backend/TextInputBase.hx
@@ -2,16 +2,16 @@ package haxe.ui.backend;
 
 class TextInputBase extends TextDisplayBase
 {
-	public var vscrollPos(get, set) : Float;
+	@:isVar public var vscrollPos(get, set) : Float;
 
 	function get_vscrollPos () : Float
 	{
-		return 0.0;
+		return vscrollPos;
 	}
 
 	function set_vscrollPos (value:Float) : Float
 	{
-		return value;
+		return value = vscrollPos;
 	}
 
 	function new ()
